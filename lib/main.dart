564b23b4a35fdbd885acc10a2f7cd73f4b4eaa8d
Pedrinho <pdr-dev@192.168.145.236>
2021-11-19 12:45:34 -0300

    Criaçao do Projeto

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PerguntaApp());
+}
+
+@override
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Olá Garaio'),
+    );
+  }
+}
