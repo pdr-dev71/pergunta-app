d854e34d068ccd8fff054987540d6831cf18c4bf
Pedrinho <pdr-dev@192.168.1.109>
2021-11-20 15:31:52 -0300

    Remoçao da pasta testes e adicao de botoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,40 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, deprecated_member_use
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
-@override
+final perguntas = [
+  'Qual a sua cor favorita',
+  'Qual a sua idade',
+];
+
+
 class PerguntaApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Olá Garaio'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+             RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+             RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
