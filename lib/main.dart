251d6df841de80443a9f669f1ed86da8c0b366fa
Pedrinho <pdr-dev@192.168.145.236>
2021-11-20 19:35:10 -0300

    Mudanca de widget stateless para stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, deprecated_member_use
-
 import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-final perguntas = [
-  'Qual a sua cor favorita',
-  'Qual a sua idade',
-];
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
 
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
+  final perguntas = [
+    'Qual a sua cor favorita',
+    'Qual a sua idade',
+  ];
 
-class PerguntaApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -19,22 +25,31 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text(perguntas[0]),
+            Text(perguntas[perguntaSelecionada]),
             RaisedButton(
               child: Text('Resposta 1'),
-              onPressed: null,
+              onPressed: responder,
             ),
-             RaisedButton(
+            RaisedButton(
               child: Text('Resposta 2'),
-              onPressed: null,
+              onPressed: responder,
             ),
-             RaisedButton(
+            RaisedButton(
               child: Text('Resposta 3'),
-              onPressed: null,
+              onPressed: responder,
             ),
           ],
         ),
       ),
     );
   }
+
+}
+
+class PerguntaApp extends StatefulWidget {
+
+  PerguntaAppState createState() {
+    return PerguntaAppState();
+  }
+
 }
